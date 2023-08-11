872e39170a72bdbd5eaecc29ad0f8318266c98a9
AboGhanem99 <awsan11111@gmail.com>
2023-08-11 13:19:16 +0500

    fix

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -1,10 +1,32 @@
+import 'animate.css';
+import { useState , useEffect} from 'react';
 
-import 'animate.css'; 
 
 const Hero: React.FC = () => {
+
+    const [mainImgUrl, setMainImgUrl] = useState('/HeroImgs/HeroMainImgMobile.webp');
+
+    const updateImageOnResize = () => {
+
+        if (window.innerWidth <= 768) {
+            setMainImgUrl("/HeroImgs/HeroMainImgMobile.webp");
+        } else {
+            setMainImgUrl("/HeroImgs/HeroMainImgDesktop.webp");
+        }
+    };
+
+    useEffect(() => {
+        updateImageOnResize(); 
+        window.addEventListener('resize', updateImageOnResize);
+
+        return () => {
+            window.removeEventListener('resize', updateImageOnResize);
+        };
+    }, []);
+
     return (
         <div className="bg-[#334A5C] md:w-full md:h-screen flex justify-around ">
-            <div id="Hero" className="w-[90%] 2xl:w-[70%] flex flex-col md:flex-row justify-center items-center ">
+            <div id="Hero" className="w-[90%] 2xl:w-[70%] flex flex-col md:flex-row justify-center items-center mt-10 md:mt-0">
 
                 <div className="md:w-[50%] md:h-full h-full bg-[url(/HeroImgs/bg.webp)] bg-no-repeat bg-contain flex flex-col justify-center animate__animated animate__rollIn">
                     <h1 className="text-[#00DFC0] md:text-2xl 2xl:text-4xl font-bold mb-4 animate__animated">Transforming Visions into Reality</h1>
@@ -12,7 +34,7 @@ const Hero: React.FC = () => {
                     <h1 className="bg-[#00DFC0] cursor-pointer w-fit px-12 py-4 text-xl 2xl:text-3xl rounded-3xl font-bold transform transition-transform md:hover:scale-125 hover:scale-110 hover:bg-[#00A89C] hover:transition  hover:duration-1000 hover:text-white">Contact Us</h1>
                 </div>
 
-                <img src="/HeroImgs/HeroMainImg.webp" alt="HeroMainImg" className="md:w-[50%] aspect-square animate__animated animate__backInDown" />
+                <img src={mainImgUrl} alt="HeroMainImg" className="md:w-[50%] md:h-auto w-[350px] h-[350px] aspect-square animate__animated animate__backInDown mt-4 md:mt-0" />
 
             </div>
         </div>
